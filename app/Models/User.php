8a2836151e9109341286275a6c9d602e3a5c3b97
osipovmn <kindly1987@gmail.com>
2024-12-10 17:09:36 +0300

    modified model and user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'data',
         'password',
     ];
 
@@ -41,8 +42,10 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
+            'data' => 'json',
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'data->custom_value' => 'bool',
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return App\Models\User::create([
+        'name' => 'Test',
+        'email' => 'test@test.com',
+        'password' => Hash::make('password'),
+        'data' => [
+            'custom_value' => 1
+        ]
+    ]);
 });
